787c8bb55f8acf06b4152f2bd49a3ab712668f4d
Sunil Kushwah <kussunil513@gmail.com>
2021-03-02 23:54:31 +0400

    initial project commit

---
+++ src/test/java/sun/springframework/springrecipeapp/SpringRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sun.springframework.springrecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
