60c1c70ae72765e36f1eadacef2bab54b3d04adb
Sunil Kushwah <kussunil513@gmail.com>
2021-03-29 23:25:03 +0400

    Added project lombok.

--- src/main/java/sun/springframework/springrecipeapp/service/impl/RecipeServiceImpl.java
+++ src/main/java/sun/springframework/springrecipeapp/service/impl/RecipeServiceImpl.java
@@ -1,5 +1,6 @@
 package sun.springframework.springrecipeapp.service.impl;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import sun.springframework.springrecipeapp.domain.Recipe;
 import sun.springframework.springrecipeapp.repositories.RecipeRepository;
@@ -8,6 +9,7 @@ import sun.springframework.springrecipeapp.service.RecipeService;
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -19,6 +21,7 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm in  the service");
         Set<Recipe> recipeSet = new HashSet<>();
         Iterable<Recipe> all = recipeRepository.findAll();
         all.iterator().forEachRemaining(recipeSet::add);
