d5f86ff1e10fb6412798f76634a7fbecb98bd946
Sunil Kushwah <kussunil513@gmail.com>
2021-05-01 01:18:18 +0400

    created mock mvc test for index controller, and created integration test for unit of measure.

---
+++ src/test/java/sun/springframework/springrecipeapp/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,43 @@
+package sun.springframework.springrecipeapp.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import sun.springframework.springrecipeapp.domain.UnitOfMeasure;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    @DirtiesContext
+    void findByDescription() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        assertEquals("Teaspoon", unitOfMeasureOptional.get().getDescription());
+
+    }
+
+    @Test
+    void findByDescriptionCup() {
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+        assertEquals("Cup", unitOfMeasureOptional.get().getDescription());
+
+    }
+}
