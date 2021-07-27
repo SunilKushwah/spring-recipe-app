c78bd9b369a6840cdd98c3ecbb5d2af05bd9ac8d
Sunil Kushwah <kussunil513@gmail.com>
2021-07-28 00:40:04 +0400

    added profile based application.properties files.

--- src/main/java/sun/springframework/springrecipeapp/boostrap/RecipeBootstrap.java
+++ src/main/java/sun/springframework/springrecipeapp/boostrap/RecipeBootstrap.java
@@ -3,6 +3,7 @@ package sun.springframework.springrecipeapp.boostrap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 @Slf4j
 @Component
+@Profile("default")
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
     private CategoryRepository categoryRepository;
